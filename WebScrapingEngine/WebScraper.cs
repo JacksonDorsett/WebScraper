a4b7d5319b27229678f4d1e7acc38928bff3ee90
Jackson Dorsett <jackson.dorsett@wsu.edu>
2020-04-04 12:59:52 -0700

    Built skeletal Webscraper Base Class

--- WebScrapingEngine/WebScraper.cs
+++ WebScrapingEngine/WebScraper.cs
@@ -10,12 +10,30 @@ namespace WebScrapingEngine
     /// Webscraper object.
     /// </summary>
     /// <typeparam name="T">Object being scraped.</typeparam>
-    public class WebScraper<T>
+    public abstract class WebScraper<T>
     {
+        /// <summary>
+        /// Objects scraped.
+        /// </summary>
+        protected List<T> ScrapedObjects { get; private set; }
+
         protected readonly ILinkScraper linkScraper;
         protected readonly IPageValidator pageValidator;
         protected readonly IPageScraper<T> scraper;
-
         protected PageHistory history;
+
+
+        protected WebScraper(ILinkScraper linkScraper, IPageScraper<T> pageScraper, IPageValidator pageValidator)
+        {
+            this.history = new PageHistory();
+            this.ScrapedObjects = new List<T>();
+        }
+
+        /// <summary>
+        /// Abstract Scraping Class.
+        /// </summary>
+        public abstract void Scrape();
+
+
     }
 }
