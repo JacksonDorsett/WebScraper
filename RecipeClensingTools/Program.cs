2c80f4f25410a2f55b43056fe0a7d312e5538367
Jackson Dorsett <jackson.dorsett@wsu.edu>
2020-04-27 19:51:59 -0700

    Added recipe clenser

---
+++ RecipeClensingTools/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeClensingTools
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
