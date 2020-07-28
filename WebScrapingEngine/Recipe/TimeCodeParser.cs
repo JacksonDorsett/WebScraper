11c212c0ba8063b2f650cfa4d3be07ce672297b3
Jackson Dorsett <dorsettj@gmail.com>
2020-07-28 15:16:31 -0700

    Created Timecode Parser and test cases

---
+++ WebScrapingEngine/Recipe/TimeCodeParser.cs
@@ -0,0 +1,35 @@
+﻿// <copyright file="TimeCodeParser.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace WebScrapingEngine.Recipe
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Parses timecode to minutes in recipe schema.
+    /// </summary>
+    internal class TimeCodeParser
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TimeCodeParser"/> class.
+        /// </summary>
+        internal TimeCodeParser()
+        {
+        }
+
+        /// <summary>
+        /// converts timecode to minutes.
+        /// </summary>
+        /// <param name="timeCode">time code input.</param>
+        /// <returns>time duration in minutes.</returns>
+        public int Parse(string timeCode)
+        {
+            return 0;
+        }
+    }
+}
